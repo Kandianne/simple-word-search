58ccf3839825cd957996b56cd13ebd7fe88b6c9b
kandianne <kandipierre@gmail.com>
2015-12-12 19:59:30 -0800

    generating answers

--- public/javascript/canvasEx.js
+++ public/javascript/canvasEx.js
@@ -1,19 +1,27 @@
-// function draw(){
-// 	var canvas = document.getElementById('drawHere')
-// 	if(canvas.getContext){
-// 		var ctx = canvas.getContext('2d');
-// 		//illustration code goes here
-// 	} else {
-// 		console.log('not rendering');
-// 	}
-// }
 
 (function createRowsThenLetterInside(){
 	for (var i = 0; i <= 10; i++){
 		document.getElementById('searchBox').innerHTML += '<tr class="randomLetters"></tr>';
+		// var rowsWithClasses = document.getElementsByClassName('randomLetters')[i].contains(classesWithAnswers);
+		// var classesWithAnswers = document.getElementsByClassName('letterToAnswer')[i].innerHTML;
+		// console.log(typeof (document.getElementsByClassName('letterToAnswer')[i].innerHTML));
+		// console.log(typeof (document.getElementsByClassName('randomLetters')[i].innerHTML));
+		// console.log(document.getElementsByClassName('letterToAnswer')[i].innerHTML);
+		// if(rowsWithClasses == 'letterToAnswer'){
+		// 	// for (var i = 0; i <= 10; i++){
+		// 		console.log('do nothing')
+		// 	// }
+		// } else {
+		// 	writeRandLettersInTable();
+		// }
+	};
+
+	for (var i = 0; i <= 30; i++) {
 		writeRandLettersInTable();
 	};
+
 })();
+
 
 function writeRandLettersInTable(){
 	//random characters are returned with this
@@ -24,6 +32,25 @@ function writeRandLettersInTable(){
 		document.getElementsByClassName('randomLetters')[i].innerHTML += '<td>' + letter + '</td>';
 	}
 };
+
+(function writeString(){
+	var dtString = ["S", "T", "R", "I", "N", "G"];
+	for (var i = 0; i <= dtString.length -1; i++){
+		document.getElementsByClassName('randomLetters')[i].innerHTML += '<td>' + dtString[i] + '</td>';
+	}
+})();
+
+
+
+// function draw(){
+// 	var canvas = document.getElementById('drawHere')
+// 	if(canvas.getContext){
+// 		var ctx = canvas.getContext('2d');
+// 		//illustration code goes here
+// 	} else {
+// 		console.log('not rendering');
+// 	}
+// }
 
 
 
