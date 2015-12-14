95dcc9357ae8c01714409a74b04ce2d049130c25
kandianne <kandipierre@gmail.com>
2015-12-13 20:24:03 -0800

    minor styles

--- public/javascript/wsEx.js
+++ public/javascript/wsEx.js
@@ -1,17 +1,13 @@
 
-
-//==============CREATING GRID OF RANDOM LETTERS=======================================
-
+//==============SUBMITTING ANSWERS=======================================
 
 function showSubmit() {
 	document.getElementById('submitBut').innerHTML = '<button onclick="submitLetters()">SUBMIT LETTERS</button>';
-	var arrayOfTds = document.getElementsByTagName('td');
-	arrayOfTds.className = 'selected'
-	// document.getElementsByTagName('td')[0].setAttribute( 'class', 'selected' );
-	console.log(arrayOfTds.index)
-	console.log(document.getElementsByClassName('selected'))
+	var arrayOfTds = document.getElementsByClassName('answerLetter');
+	console.log(arrayOfTds);
 };
 
+//==============CREATING GRID OF RANDOM LETTERS=======================================
 
 var cols = 20; 
 var rows = 10; 
@@ -29,7 +25,6 @@ for(var i = 0; i <= rows; i++) {
 }
 
 document.getElementById('wsBox').innerHTML += html;
-console.log(document.getElementById('wsBox').children[0].cells[3])
 //==============REPLACING LETTERS ON GRID WITH ANSWERS=======================================
 
 //cells are columns, rows are children.
@@ -44,38 +39,34 @@ var dtString = 'STRING';
 dtString = dtString.split('')
 for(var i = 0; i <= dtString.length - 1; i++){
 	//FROM TOP DOWN
-	document.getElementById('wsBox').children[i+1].cells[i-i].innerHTML = '<td class="answerLetter" onclick="showSubmit()">' + dtString[i] + '</td>';
-	// document.getElementById('wsBox').childNodes[i].cells[i-i] = '<td style="color:blue;">' + dtString[i] + '</td>';
+	document.getElementById('wsBox').children[i+1].cells[i-i].outerHTML = '<td class="answerLetter" onclick="showSubmit()">' + dtString[i] + '</td>';
 }
 
 var dtNumber = 'NUMBER';
 dtNumber = dtNumber.split('')
 for(var i = 0; i <= dtNumber.length - 1; i++){
 	//FROM TOP DOWN AT ANY CELL
-	document.getElementById('wsBox').children[i+1].cells[i + (17-i)].innerHTML = '<td class="answerLetter" onclick="showSubmit()">' + dtNumber[i] + '</td>';
+	document.getElementById('wsBox').children[i+1].cells[i + (17-i)].outerHTML = '<td class="answerLetter" onclick="showSubmit()">' + dtNumber[i] + '</td>';
 }
 
 var dtObject = 'TCEJBO';
 dtObject = dtObject.split('')
 for(var i = 0; i <= dtObject.length - 1; i++){
-	//FROM TOP DOWN AT ANY CELL
-	document.getElementById('wsBox').children[i+2].cells[i + (7-i)].innerHTML = '<td class="answerLetter" onclick="showSubmit()">' + dtObject[i] + '</td>';
+	document.getElementById('wsBox').children[i+2].cells[i + (7-i)].outerHTML = '<td class="answerLetter" onclick="showSubmit()">' + dtObject[i] + '</td>';
 }
 
 var dtNull = 'NULL';
 dtNull = dtNull.split('')
 for(var i = 0; i <= dtNull.length - 1; i++){
-	//FROM TOP DOWN AT ANY CELL
-	document.getElementById('wsBox').children[(i-i)+9].cells[i].innerHTML = '<td class="answerLetter" onclick="showSubmit()">' + dtNull[i] + '</td>';
+	document.getElementById('wsBox').children[(i-i)+9].cells[i].outerHTML = '<td class="answerLetter" onclick="showSubmit()">' + dtNull[i] + '</td>';
 }
 
 var dtUndefined = 'DENIFEDNU';
 dtUndefined = dtUndefined.split('')
 for(var i = 0; i <= dtUndefined.length - 1; i++){
-	document.getElementById('wsBox').children[i+1].cells[i + (11-i)].innerHTML = '<td class="answerLetter" onclick="showSubmit()">' + dtUndefined[i] + '</td>';
+	document.getElementById('wsBox').children[i+1].cells[i + (11-i)].outerHTML = '<td class="answerLetter" onclick="showSubmit()">' + dtUndefined[i] + '</td>';
 }
 
-// console.log(document.getElementById('wsBox').childNodes[6].cells[7])
 
 
 // var firstCol = [];
