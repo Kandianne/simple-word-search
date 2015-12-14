4a92fd7cda8ed8ba096d806fddbef4f711ee201a
kandianne <kandipierre@gmail.com>
2015-12-13 18:57:35 -0800

    added classes to answer tds

--- public/javascript/wsEx.js
+++ public/javascript/wsEx.js
@@ -5,8 +5,11 @@
 
 function showSubmit() {
 	document.getElementById('submitBut').innerHTML = '<button onclick="submitLetters()">SUBMIT LETTERS</button>';
-	document.getElementsByTagName('td')[0].setAttribute( 'class', 'selected' );
-	console.log(document.getElementsByTagName('td'))
+	var arrayOfTds = document.getElementsByTagName('td');
+	arrayOfTds.className = 'selected'
+	// document.getElementsByTagName('td')[0].setAttribute( 'class', 'selected' );
+	console.log(arrayOfTds.index)
+	console.log(document.getElementsByClassName('selected'))
 };
 
 
@@ -26,7 +29,7 @@ for(var i = 0; i <= rows; i++) {
 }
 
 document.getElementById('wsBox').innerHTML += html;
-
+console.log(document.getElementById('wsBox').children[0].cells[3])
 //==============REPLACING LETTERS ON GRID WITH ANSWERS=======================================
 
 //cells are columns, rows are children.
@@ -34,14 +37,14 @@ var dtBoolean = 'BOOLEAN';
 dtBoolean = dtBoolean.split('')
 for(var i = 0; i <= dtBoolean.length - 1; i++){
 	//DIAGONAL DIRECTION FROM TOP LEFT DOWN
-	document.getElementById('wsBox').children[i].cells[i+3].innerHTML = dtBoolean[i];
+	document.getElementById('wsBox').children[i].cells[i+3].outerHTML = '<td class="answerLetter" onclick="showSubmit()">' + dtBoolean[i] + '</td>';
 }
 
 var dtString = 'STRING';
 dtString = dtString.split('')
 for(var i = 0; i <= dtString.length - 1; i++){
 	//FROM TOP DOWN
-	document.getElementById('wsBox').children[i+1].cells[i-i].innerHTML = dtString[i];
+	document.getElementById('wsBox').children[i+1].cells[i-i].innerHTML = '<td class="answerLetter" onclick="showSubmit()">' + dtString[i] + '</td>';
 	// document.getElementById('wsBox').childNodes[i].cells[i-i] = '<td style="color:blue;">' + dtString[i] + '</td>';
 }
 
@@ -49,27 +52,27 @@ var dtNumber = 'NUMBER';
 dtNumber = dtNumber.split('')
 for(var i = 0; i <= dtNumber.length - 1; i++){
 	//FROM TOP DOWN AT ANY CELL
-	document.getElementById('wsBox').children[i+1].cells[i + (17-i)].innerHTML = dtNumber[i];
+	document.getElementById('wsBox').children[i+1].cells[i + (17-i)].innerHTML = '<td class="answerLetter" onclick="showSubmit()">' + dtNumber[i] + '</td>';
 }
 
 var dtObject = 'TCEJBO';
 dtObject = dtObject.split('')
 for(var i = 0; i <= dtObject.length - 1; i++){
 	//FROM TOP DOWN AT ANY CELL
-	document.getElementById('wsBox').children[i+2].cells[i + (7-i)].innerHTML = dtObject[i];
+	document.getElementById('wsBox').children[i+2].cells[i + (7-i)].innerHTML = '<td class="answerLetter" onclick="showSubmit()">' + dtObject[i] + '</td>';
 }
 
 var dtNull = 'NULL';
 dtNull = dtNull.split('')
 for(var i = 0; i <= dtNull.length - 1; i++){
 	//FROM TOP DOWN AT ANY CELL
-	document.getElementById('wsBox').children[(i-i)+9].cells[i].innerHTML = dtNull[i];
+	document.getElementById('wsBox').children[(i-i)+9].cells[i].innerHTML = '<td class="answerLetter" onclick="showSubmit()">' + dtNull[i] + '</td>';
 }
 
 var dtUndefined = 'DENIFEDNU';
 dtUndefined = dtUndefined.split('')
 for(var i = 0; i <= dtUndefined.length - 1; i++){
-	document.getElementById('wsBox').children[i+1].cells[i + (11-i)].innerHTML = dtUndefined[i];
+	document.getElementById('wsBox').children[i+1].cells[i + (11-i)].innerHTML = '<td class="answerLetter" onclick="showSubmit()">' + dtUndefined[i] + '</td>';
 }
 
 // console.log(document.getElementById('wsBox').childNodes[6].cells[7])
